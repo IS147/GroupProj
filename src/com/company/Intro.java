3cbfb185615595399da20d08fe1de1ed8a7bea23
Cole <cfelch1@umbc.edu>
2022-05-11 19:28:23 -0400

    Final

--- src/com/company/Intro.java
+++ src/com/company/Intro.java
@@ -7,7 +7,7 @@ public class Intro {
             System.out.println("Welcome to the Restock Notifier Program!");
             System.out.println();
             System.out.println("As a demonstration on how this program works, a list of stock numbers on common groceries wil be given,");
-            System.out.println("and customers will begin to purchase items daily and as quantity for a product reaches below 15 a restock notification will appear.");
+            System.out.println("and customers will begin to purchase items daily and as quantity for a product reaches below 20 a restock notification will appear.");
             System.out.println("---------------------------------------------------");
         }
     }
